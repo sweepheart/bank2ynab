db9438d6316b41743cc8481d8091783da60c97fa
Torben Gundtofte-Bruun <torbengb@users.noreply.github.com>
2018-03-02 13:14:41 +0100

    DESCRIPTION

--- __init__.py
+++ __init__.py
@@ -4,10 +4,11 @@
 
 # Package meta-data:
 NAME = "bank2ynab"
-DESCRIPTION = "Convert CSV from various banks' export formats into " \
+DESCRIPTION = "Convert CSV files from various banks into YNAB's import " \
+        "format."
+LONG_DESCRIPTION = "bank2ynab is a common project to consolidate all " \
+        "conversion efforts from various banks' CSV export formats into " \
         "YNAB's import format."
-LONG_DESCRIPTION = "A common project to consolidate all conversion efforts " \
-        "from various banks' export formats into YNAB's import format."
 URL = "https://github.com/torbengb/bank2ynab"
 EMAIL = "torben@g-b.dk"
 AUTHOR = "https://github.com/torbengb/bank2ynab/graphs/contributors"
