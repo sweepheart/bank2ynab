45bca3f59a552378f7ccdfdbc2de8d5740f3eed5
Torben Gundtofte-Bruun <torbengb@users.noreply.github.com>
2018-03-02 10:48:50 +0100

    flake8 corrections

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# created for Issue #176 based on 
+# created for Issue #176 based on
 # https://packaging.python.org/tutorials/distributing- \
 # packages/#configuring-your-project
 
@@ -25,11 +25,11 @@ VERSION = "1.0.0.dev201803020943"
 # I'd like the suffix to *automagically* include the GitHub branch name,
 # but that will have to be a separate enhancement, see Issue #180.
 
-# only add the suffix to the patch level if it's not empty 
+# only add the suffix to the patch level if it's not empty
 # and not a master release:
 # if (version_suffix != "") and (version_suffix != "master"):
 #    version_patch = version_patch + '-' + version_suffix
-## NOT ALLOWED: VERSION NUMBER MUST BE ONLY DIGITS and . and _ and -.
+# NOT ALLOWED: VERSION NUMBER MUST BE ONLY DIGITS and . and _ and -.
 
 # version number according to
 # https://codereview.stackexchange.com/a/131490 :
