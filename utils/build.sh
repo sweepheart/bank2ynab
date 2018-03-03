945d85157d44af3f98e4c55cb651f73811ddcbef
Torben Gundtofte-Bruun <torben@g-b.dk>
2018-03-03 22:31:29 +0100

    added uninstall+reinstall to the build script
    also removed stray build.sh in the root.

--- utils/build.sh
+++ utils/build.sh
@@ -6,7 +6,7 @@ clear
 get_input ()
 { if [ -z "$2" ] # Is parameter #2 zero length?
   then
-    echo "I didn't get 2 parameters. Exiting." ; exit
+    echo "I didn't get 2 parameters. Exiting." ; echo "" ; exit
   else
     while true; do
       read -p "$1 Press 'Y' to continue, or any other key to skip: " -n 1 response
@@ -15,26 +15,38 @@ get_input ()
           $2
           result=$?
           if [ $result -eq ""0"" ]; then 
-            echo "--OK--"
+            echo "" ; echo "--OK--" ; echo ""
             break
           else
-            echo "An error occurred: " $result ". Exiting!"
+            echo "" ; echo "An error occurred: " $result ". Exiting!" ; echo ""
             exit
           fi
           ;; # yes I know that 'break' is never going to run, but it feels proper to have it there.
-        *     ) echo "";echo "Nothing done."; break;;
+        *     ) echo "" ; echo "Nothing done."; echo "" ; break;;
       esac
     done
   fi
 }
-# remove old build files:
-if [ -f ""dist/*"" ] 
-    then 
-      echo "rm..."
-      rm dist/*
-fi
+
 # do the actual work:
+
+# remove old build files:
+if [ -f "dist/*" ] 
+then rm dist/*
+fi
+
 get_input "Are you ready to BUILD the package?" "python setup.py bdist_wheel"
-get_input "Are you ready to UPLOAD the package?" "twine upload --repository-url https://test.pypi.org/legacy/ dist/*"
-echo ""
-exit
+
+get_input "Are you ready to UPLOAD the package?" "twine upload -u bank2ynab_username --repository-url https://test.pypi.org/legacy/ dist/*"
+if [ $response == ""y"" ]
+then echo "Package will be listed at https://test.pypi.org/project/bank2ynab/#history in a few minutes." ; echo ""
+fi
+
+get_input "Wait 2 minutes and install new build?" " "
+if [ $response == ""y"" ]; then
+  echo "Removing..." ; echo ""
+  sudo -H pip uninstall -y bank2ynab
+  echo "Waiting, then installing..."
+  sleep 120
+  sudo -H pip install --no-cache-dir --index-url https://test.pypi.org/simple/ bank2ynab
+fi
