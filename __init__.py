85e7e420ad6151fc79e89a2901298734b4447747
Torben Gundtofte-Bruun <torbengb@users.noreply.github.com>
2018-03-06 16:27:29 +0100

    Reworked suffix specification to be PyPI compliant.

--- __init__.py
+++ __init__.py
@@ -15,33 +15,9 @@ AUTHOR = "https://github.com/torbengb/bank2ynab/graphs/contributors"
 KEYWORDS = "ynab bank bank-statement conversion-efforts credit-card" \
         "transactions converter ynab-converter ynab-format money csv" \
         "bank-format conversion conversion-utility"
-VERSION = "1.0.1000013"  # "1.0.0.dev201803041527"
 
-# define the version numbers:
-# !! See also: https://github.com/torbengb/bank2ynab/wiki/VersionNumbers !!
-# version_major = 1  # must be integer
-# version_minor = 0  # must be integer
-# version_patch = 1000003  # must be integer
-# version_suffix = "feature-176-pypi-installer"
-# I'd like the suffix to *automagically* include the GitHub branch name,
-# but that will have to be a separate enhancement, see Issue #180.
+# !! See https://github.com/torbengb/bank2ynab/wiki/VersionNumbers
+# !! for details about correct version number values!
+# Hyphens are not allowed!
 
-# only add the suffix to the patch level if it's not empty
-# and not a master release:
-# if (version_suffix != "") and (version_suffix != "master"):
-#    version_patch = version_patch + '-' + version_suffix
-# NOT ALLOWED: VERSION NUMBER MUST BE ONLY DIGITS and . and _ and -.
-
-# version number according to
-# https://codereview.stackexchange.com/a/131490 :
-# version_info = (version_major, version_minor, version_patch)
-# VERSION = '.'.join(str(c) for c in version_info)
-
-# Apparently, version numbers must follow this format:
-# [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
-# sources:
-# https://packaging.python.org/tutorials/distributing-packages/#standards- \
-# compliance-for-interoperability
-# https://www.python.org/dev/peps/pep-0440/#public-version-identifiers
-# Notes:
-# Hyphens are not allowed! That's unfortunate, as the branches contain them.
+VERSION = "1.0.1a.dev201803041527"
